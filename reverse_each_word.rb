74bfefdf23bb9d489466d533bbfe859ed534237d
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 05:46:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
 array = []
 string2 = string.reverse
 string3 = string2.split(" ")
-string3.each do |word|
+string3.collect do |word|
   array.unshift(word)
 end
 array.join(" ")
